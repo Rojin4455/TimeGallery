6587570378da2ff21c5cab6d6ac86e9320f92587
Rojin Saji <rojinsaji@Rojins-MacBook-Air.local>
2025-01-23 16:14:02 +0530

    add absolute static root path

--- First_Ecommerce/settings.py
+++ First_Ecommerce/settings.py
@@ -140,7 +140,7 @@ STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
 
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = BASE_DIR / 'First_Ecommerce' / 'staticfiles' 
 
 
 # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
