ad4e565cdbb27918e0ade8957917dd2453cc12f4
Rojin Saji <rojinsaji@Rojins-MacBook-Air.local>
2025-01-28 15:29:43 +0530

    added origin of the new aws account instance (rojinrenwin66-account)

--- First_Ecommerce/settings.py
+++ First_Ecommerce/settings.py
@@ -20,8 +20,8 @@ BASE_API_URL = os.getenv("BASE_API_URL")
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','timegallery.store','localhost','43.204.130.125']
-CSRF_TRUSTED_ORIGINS = ['https://api.razorpay.com','https://timegallery.store', 'http://43.204.130.125', 'https://43.204.130.125']
+ALLOWED_HOSTS = ['127.0.0.1','timegallery.store','localhost','43.204.130.125', '13.233.137.144']
+CSRF_TRUSTED_ORIGINS = ['https://api.razorpay.com','https://timegallery.store', 'http://43.204.130.125', 'https://43.204.130.125', 'http://13.233.137.144', 'https://13.233.137.144']
 
 LOGIN_REDIRECT_URL = '/'
 
